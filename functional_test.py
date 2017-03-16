3d99d5f48920b684cfe1231cc962f3c344a0ded5
Hanju Jo <gkswn121@gmail.com>
2017-03-16 23:41:03 +0900

    템플릿을 이용한 메인 페이지 HTML 생성

--- functional_test.py
+++ functional_test.py
@@ -26,7 +26,7 @@ class NewVisitorTest(unittest.TestCase):
         # She is invited to enter a to-do item straight away.
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-            inputbox.get_attribute('placeholder', 'Enter new item')
+            inputbox.get_attribute('placeholder'), 'Enter new item'
         )
 
         # She types "Buy peacock feathers" into a text box.
@@ -39,7 +39,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New item did not appear in table"
         )
 
         # There is still a text box inviting her to add another item.
