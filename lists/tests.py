92819fbd391c615f2e1a3266b0ed4e25ee5f6014
Hanju Jo <gkswn121@gmail.com>
2017-03-17 13:12:56 +0900

    A new Item model and migration for it

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,9 @@ from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
+
 from lists.views import home_page
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
@@ -40,3 +42,23 @@ class HomePageTest(TestCase):
         response_decode = self.remove_csrf(response.content.decode())
 
         self.assertEqual(response_decode, expected_html)
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'First item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Second item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'First item')
+        self.assertEqual(second_saved_item.text, 'Second item')
