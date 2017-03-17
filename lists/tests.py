bb5cd5489bb782a6f9dec06fa00db28917fe71c9
Hanju Jo <gkswn121@gmail.com>
2017-03-17 13:40:01 +0900

    Split into two unit test for POST

--- lists/tests.py
+++ lists/tests.py
@@ -37,6 +37,13 @@ class HomePageTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new item')
 
+    def test_home_page_redirects_after_post(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new item'
+
+        response = home_page(request)
+
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/')
 
