47b1f8ff6c83d5e419e6b13d7664567194780ea4
Hanju Jo <gkswn121@gmail.com>
2017-03-17 15:04:58 +0900

    Home page now shows all list items

--- lists/tests.py
+++ lists/tests.py
@@ -52,6 +52,16 @@ class HomePageTest(TestCase):
         home_page(request)
         self.assertEqual(Item.objects.count(), 0)
 
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
 
 class ItemModelTest(TestCase):
 
