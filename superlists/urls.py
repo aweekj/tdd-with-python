aceeb26eb8609a268fe1293937c690b476526f7f
Hanju Jo <gkswn121@gmail.com>
2017-03-23 23:14:31 +0900

    Refactoring urls

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,9 +1,7 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from lists import views
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
-    url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
-    url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
-    url(r'^lists/new$', views.new_list, name='new_list')
+    url(r'^lists/', include('lists.urls')),
 ]
