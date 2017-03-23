32dc8bf7db14efd4464c1578364dac054385d5ab
Hanju Jo <gkswn121@gmail.com>
2017-03-23 22:09:07 +0900

    Change unit test for list model

--- lists/tests.py
+++ lists/tests.py
@@ -5,7 +5,7 @@ from django.template.loader import render_to_string
 from django.test import TestCase
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -34,8 +34,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
@@ -59,16 +60,24 @@ class ListViewTest(TestCase):
         self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'First item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Second item'
+        second_item.list = list_
         second_item.save()
+
+        saved_lists = List.objects.first()
+        self.assertEqual(saved_lists, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -76,4 +85,6 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'First item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Second item')
+        self.assertEqual(second_saved_item.list, list_)
