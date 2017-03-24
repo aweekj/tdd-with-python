a993b7ea5d2b3b27e78efa3533a8268e1df16361
Hanju Jo <gkswn121@gmail.com>
2017-03-24 21:24:22 +0900

    First ft for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -104,3 +104,16 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep.
+
+    def test_layout_and_styling(self):
+        # Edith visits to main page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notice that the input box is at the middle of the window
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
