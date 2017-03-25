bccde3925faa55b4cef09cd219c640734184f892
Hanju Jo <gkswn121@gmail.com>
2017-03-26 01:23:55 +0900

    Refactoring urls

--- lists/views.py
+++ lists/views.py
@@ -10,13 +10,12 @@ def home_page(request):
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
     error = None
-
     if request.method == 'POST':
         try:
             item = Item(text=request.POST['item_text'], list=list_)
             item.full_clean()
             item.save()
-            return redirect('/lists/%d/' % (list_.id,))
+            return redirect(list_)
         except ValidationError:
             error = "You can't have an empty list item"
     return render(request, 'list.html', {'list': list_, 'error': error})
@@ -32,5 +31,5 @@ def new_list(request):
         list_.delete()
         error = "You can't have an empty list item"
         return render(request, 'home.html', {"error": error})
-    return redirect('/lists/%d/' % (list_.id, ))
+    return redirect(list_)
 
