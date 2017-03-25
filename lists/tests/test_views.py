1b0fc75014beb0a7b060a17ecabbc8816fcf6695
Hanju Jo <gkswn121@gmail.com>
2017-03-26 00:55:45 +0900

    Make view_list to save items and delete add_item in view

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -42,14 +42,11 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/%d/' % correct_list.id, )
         self.assertEqual(response.context['list'], correct_list)
 
-
-class NewItemTest(TestCase):
-
-    def test_can_save_a_post_request_to_an_existing_list(self):
+    def test_can_save_a_POST_request_to_an_existing_list(self):
         correct_list = List.objects.create()
 
         self.client.post(
-            '/lists/%d/add_item' % (correct_list.id, ),
+            '/lists/%d/' % (correct_list.id, ),
             data={'item_text': 'new item to existing list'}
         )
 
@@ -58,11 +55,11 @@ class NewItemTest(TestCase):
         self.assertEqual(new_item.text, 'new item to existing list')
         self.assertEqual(new_item.list, correct_list)
 
-    def test_redirects_to_list_view(self):
+    def test_POST_redirects_to_list_view(self):
         correct_list = List.objects.create()
 
         response = self.client.post(
-            '/lists/%d/add_item' % (correct_list.id, ),
+            '/lists/%d/' % (correct_list.id, ),
             data={'item_text': 'new item to existing list'}
         )
 
--- lists/views.py
+++ lists/views.py
@@ -9,6 +9,9 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect('/lists/%d/' % (list_.id,))
     return render(request, 'list.html', {'list': list_})
 
 
@@ -24,8 +27,3 @@ def new_list(request):
         return render(request, 'home.html', {"error": error})
     return redirect('/lists/%d/' % (list_.id, ))
 
-
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/%d/' % (list_.id, ))
