5c72bf76402547951ca18d924720a0f5ecdb5a44
Hanju Jo <gkswn121@gmail.com>
2017-03-25 23:23:46 +0900

    Devide unittests into two files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -56,36 +56,6 @@ class ListViewTest(TestCase):
         self.assertEqual(response.context['list'], correct_list)
 
 
-class ListAndItemModelTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = 'First item'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Second item'
-        second_item.list = list_
-        second_item.save()
-
-        saved_lists = List.objects.first()
-        self.assertEqual(saved_lists, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'First item')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'Second item')
-        self.assertEqual(second_saved_item.list, list_)
-
-
 class NewItemTest(TestCase):
 
     def test_saving_a_post_request(self):
