03f0bf1f5c67a82a8fe4ff9ad606b71e79fad0d0
Hanju Jo <gkswn121@gmail.com>
2017-03-29 21:55:27 +0900

    Use form in all views

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -26,8 +26,7 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys(Keys.ENTER)
 
         # She receives a similar warning on the list page
-        with self.wait_for_page_load(timeout=10):
-            self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('1: Buy milk')
 
         error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text, "You can't have an empty list item")
